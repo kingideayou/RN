b4580a210aa6f328db1fc830c98253f6b0a2a9f6
NeXT___ <kingideayou@foxmail.com>
2017-03-16 13:56:53 +0800

    State(状态)

--- index.android.js
+++ index.android.js
@@ -42,6 +42,8 @@ export default class AwesomeProject extends Component {
 }
 */
 
+//Props(属性)
+/*
 class Greeting extends Component {
   render() {
     return (
@@ -61,7 +63,39 @@ export default class AwesomeProject extends Component {
     );
   }
 }
+*/
 
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true};
+
+    //每 1000ms 对 shouwText 状态取反
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : '';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <View>
+          <Blink text='I love to blink' />
+          <Blink text='Yes blinking is so great' />
+          <Blink text='Why did they ever take this out of HTML' />
+          <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
