75bf9c81325fc20318127d7123e536a288f8365c
NeXT___ <kingideayou@foxmail.com>
2017-03-16 14:22:31 +0800

    width、height & flex

--- index.android.js
+++ index.android.js
@@ -101,6 +101,7 @@ export default class AwesomeProject extends Component {
 */
 
 //Style 样式
+/*
 class LotsOfStyles extends Component {
   render() {
     return (
@@ -113,11 +114,41 @@ class LotsOfStyles extends Component {
     );
   }
 }
+*/
+
+class FixedDimensionsBasics extends Component {
+  render() {
+    return (
+      // width height
+      /*
+      <View>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+          <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      </View>
+      */
+
+      //flex
+      // 试试去掉父View中的`flex: 1`。
+      // 则父View不再具有尺寸，因此子组件也无法再撑开。
+      // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+      <View style={{flex: 1}}>
+        <View style={{flex:1, backgroundColor: 'powderblue'}} />
+        <View style={{flex:2, backgroundColor: 'skyblue'}} />
+        <View style={{flex:3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
 
 export default class AwesomeProject extends Component {
   render() {
     return (
-        <LotsOfStyles/>
+      //Style
+        // <LotsOfStyles/>
+
+        //FixedDimensions
+        <FixedDimensionsBasics/>
     );
   }
 }
