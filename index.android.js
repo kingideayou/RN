07f65b8d043d7edad38745b88ebd8440e73a4afb
NeXT___ <kingideayou@foxmail.com>
2017-03-16 13:43:44 +0800

    feature: props(属性)

--- index.android.js
+++ index.android.js
@@ -9,15 +9,25 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
+/*
 export default class AwesomeProject extends Component {
   render() {
+
+    let picUrl = {
+      uri: 'http://7xi8d6.com1.z0.glb.clouddn.com/2017-03-09-17127109_1652837611687612_1425055271046086656_n.jpg'
+    };
+
     return (
       <View style={styles.container}>
+
+        <Image source={picUrl} style={{width: 193, height: 110}} />
+
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native !!!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
@@ -30,6 +40,28 @@ export default class AwesomeProject extends Component {
     );
   }
 }
+*/
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='小一' />
+        <Greeting name='小二' />
+        <Greeting name='小三' />
+      </View>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
